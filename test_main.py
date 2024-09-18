a22ed56984975209e4c579b14462a0f4d47bddb2
ElkinD18 <elkin060618chulca@gmail.com>
2024-09-17 19:58:13 -0500

    Implementadas funciones básicas y pruebas unitarias para la calculadora

--- test_main.py
+++ test_main.py
@@ -1,27 +1,27 @@
 import unittest
-import main
+import operaciones
 
 class TestCalculadora(unittest.TestCase):
     def test_sumar(self):
-        self.assertEqual(main.sumar(3, 5), 8)
-        self.assertEqual(main.sumar(-1, 1), 0)
-        self.assertEqual(main.sumar(0, 0), 0)
+        self.assertEqual(operaciones.suma(3, 5), 8)
+        self.assertEqual(operaciones.suma(-1, 1), 0)
+        self.assertEqual(operaciones.suma(0, 0), 0)
 
     def test_restar(self):
-        self.assertEqual(main.restar(5, 3), 2)
-        self.assertEqual(main.restar(-1, 1), -2)
-        self.assertEqual(main.restar(0, 0), 0)
+        self.assertEqual(operaciones.resta(5, 3), 2)
+        self.assertEqual(operaciones.resta(-1, 1), -2)
+        self.assertEqual(operaciones.resta(0, 0), 0)
 
     def test_multiplicar(self):
-        self.assertEqual(main.multiplicar(3, 5), 15)
-        self.assertEqual(main.multiplicar(-1, 1), -1)
-        self.assertEqual(main.multiplicar(0, 0), 0)
+        self.assertEqual(operaciones.multiplicacion(3, 5), 15)
+        self.assertEqual(operaciones.multiplicacion(-1, 1), -1)
+        self.assertEqual(operaciones.multiplicacion(0, 0), 0)
 
     def test_dividir(self):
-        self.assertEqual(main.dividir(10, 2), 5)
-        self.assertEqual(main.dividir(5, 2), 2.5)
-        self.assertEqual(main.dividir(8, 4), 2)
-        self.assertEqual(main.dividir(5, 0), "Error: No se puede dividir por cero")
+        self.assertEqual(operaciones.division(10, 2), 5)
+        self.assertEqual(operaciones.division(5, 2), 2.5)
+        self.assertEqual(operaciones.division(8, 4), 2)
+        self.assertEqual(operaciones.division(5, 0), "Error: No se puede dividir por cero")
 
-if __name__ == "__main__":
-    unittest.main()
+if __name__ == "__operaciones__":
+    unittest.operaciones()
